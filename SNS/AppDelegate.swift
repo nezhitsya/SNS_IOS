0714bf960822b7d5c5b4ca2b9f82be7586d0944c
nezhitsya <huyn9295@naver.com>
2021-02-18 10:44:02 +0900

    set up firebase & sign up

--- SNS/AppDelegate.swift
+++ SNS/AppDelegate.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
@@ -78,4 +79,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
--- SNS/SignUpViewController.swift
+++ SNS/SignUpViewController.swift
@@ -6,16 +6,55 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var email: SignUpTextField!
+    @IBOutlet weak var password: SignUpTextField!
+    @IBOutlet weak var signUp: UIButton!
+    
+    var databaseRef = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signUp.isEnabled = false
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func didTapSignup(_ sender: Any) {
+        
+        signUp.isEnabled = false
+        
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: { (user, error) in
+            
+            if(error == nil) {
+                Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!, completion: { (user, error) in
+                    
+                    if(error == nil) {
+                        self.databaseRef.child("User").child((user?.user.uid)!).child("email").setValue(self.email.text!)
+                        
+                        self.performSegue(withIdentifier: "HandleViewSegue", sender: nil)
+                    }
+                })
+            }
+        })
+    }
+    
+    @IBAction func didTextChange(_ sender: UITextField) {
+        
+        if(email.text!.count > 0 && password.text!.count > 0) {
+            signUp.isEnabled = true
+        } else {
+            signUp.isEnabled = false
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
