dd257d60acb8d5cf753f1b2ffebe41f55c25a423
nezhitsya <huyn9295@naver.com>
2021-02-23 10:50:02 +0900

    set up profile pic

--- SNS/ProfileViewController.swift
+++ SNS/ProfileViewController.swift
@@ -8,17 +8,76 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var likesContainer: UIView!
     @IBOutlet weak var mediaContainer: UIView!
     @IBOutlet weak var postContainer: UIView!
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var about: UITextField!
+    
+    var databaseRef = Database.database().reference()
+    var storageRef = Storage.storage().reference()
+    var user: AnyObject? = .none
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.user = Auth.auth().currentUser
+        
+        let userUid = Auth.auth().currentUser?.uid
+        self.databaseRef.child("User").child(userUid!).observeSingleEvent(of: .value) { ( snapshot: DataSnapshot) in
+            
+            self.name.text = snapshot.value(forKey: "nickname") as? String
+            self.nickname.text = snapshot.value(forKey: "nickname") as? String
+            
+            if(snapshot.value(forKey: "description") != nil) {
+                self.about.text = snapshot.value(forKey: "about") as? String
+            }
+            
+            if(snapshot.value(forKey: "profilePic") != nil) {
+                let databaseProfilePic = snapshot.value(forKey: "profilePic") as! String
+                
+                let data: Data = try! Data(contentsOf: URL(string: databaseProfilePic)!)
+                
+                self.setProfilePicture(imageView: self.profilePicture, imageToSet: UIImage(data: data)!)
+                
+            }
+        }
+    }
+    
+    @IBAction func didTapProfilePicture(_ sender: UITapGestureRecognizer) {
+        let myActionSheet = UIAlertController(title: "Profile Picture", message: "Select", preferredStyle: UIAlertController.Style.actionSheet)
+        let viewPicture = UIAlertAction(title: "Picture", style: UIAlertAction.Style.default) { (action) in
+            
+            let imageView = sender.view as! UIImageView
+            let newImageView = UIImageView(image: imageView.image)
+            
+            newImageView.frame = self.view.frame
+            newImageView.backgroundColor = UIColor.white
+            newImageView.contentMode = .scaleAspectFit
+            newImageView.isUserInteractionEnabled = true
+            
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissFullScreenImage))
+            
+            newImageView.addGestureRecognizer(tap)
+            self.view.addSubview(newImageView)
+        }
+        
+        let photoGallery = UIAlertAction(title: "Photos", style: UIAlertAction.Style.default) { (action) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.savedPhotosAlbum) {
+                self.imagePicker.delegate = self
+                self.imagePicker.sourceType = UIImagePickerController.SourceType.savedPhotosAlbum
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+        }
     }
     
     @IBAction func didTapLogout(_ sender: Any) {
@@ -27,6 +86,8 @@ class ProfileViewController: UIViewController {
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         
         let welcomeViewController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "welcomeViewController")
+        
+        welcomeViewController.modalPresentationStyle = .fullScreen
         
         self.present(welcomeViewController, animated: true, completion: nil)
     }
@@ -52,6 +113,17 @@ class ProfileViewController: UIViewController {
                 self.likesContainer.alpha = 1
             })
         }
+    }
+    
+    @objc func dismissFullScreenImage(sender: UITapGestureRecognizer) {
+        sender.view?.removeFromSuperview()
+    }
+    
+    internal func setProfilePicture(imageView: UIImageView, imageToSet: UIImage) {
+        imageView.layer.cornerRadius = 10.0
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.masksToBounds = true
+        imageView.image = imageToSet
     }
     
     /*
