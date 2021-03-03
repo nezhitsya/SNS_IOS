4367f6c74914b0b18e035b7b20cd6604168f7bd6
nezhitsya <huyn9295@naver.com>
2021-03-03 10:19:00 +0900

    set up user list

--- SNS/FollowUsersTableViewController.swift
+++ SNS/FollowUsersTableViewController.swift
@@ -38,10 +38,6 @@ class FollowUsersTableViewController: UITableViewController, UISearchResultsUpda
                 self.followUsersTableView.insertRows(at: [IndexPath(row: self.usersArray.count - 1, section: 0)], with: UITableView.RowAnimation.automatic)
             }
         })
-
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -82,41 +78,6 @@ class FollowUsersTableViewController: UITableViewController, UISearchResultsUpda
 //            }
 //        }
 //    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     func updateSearchResults(for searchController: UISearchController) {
         
